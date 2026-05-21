38a41eb29ed02bce2aba181d927e970a1c3c7f7b
Arthur <arthurndubi5@gmail.com>
2026-05-21 15:21:57 +0300

    Completed validation of 300 tickers that I will use to fetch dividend data.

---
+++ etl_pipeline/src/extract/_fetch_dividend_data.py
@@ -0,0 +1,104 @@
+import pandas as pd
+import random
+import time
+import pandera.pandas as pa
+import pandas_market_calendars as mcal
+from itertools import islice 
+from datetime import timedelta 
+
+
+from config.logging_config import get_logger
+from etl_pipeline.src.schema.ticker_schemas import CURRENT_PRICE_FILE_SCHEMA
+from config.settings import DATA_COLS
+
+logger = get_logger(__name__)
+
+def validate_dividend_tickers(df):
+    """Validating the inputs of 300 tickers and comfirming the data is 
+    OK."""
+
+    #Check/confirm the data is not None.
+
+    if df is None:
+        raise ValueError("The input provided is None.")
+
+    #Check/confirm datatype
+
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError(f"Expected a pandas dataframe but got {type(df).__name__}")
+    
+    #Check/confirm if the dataframe is empty
+
+    if df.empty:
+        raise ValueError(f"The dataframe is empty.")
+    
+    #Check/confirm row counts
+
+    n_rows= len(df)
+
+    if n_rows < 110:
+        raise ValueError(f" Dataframe has only {n_rows} rows. Minimum required is 110 rows")
+           
+    elif 110 <= n_rows <=249:
+        logger.warning(f"Dataframe has {n_rows} rows. This is below the ideal range of 250-300.")
+        f"Received {n_rows} rows."
+
+    else:
+        pass 
+
+    #Pandera validation
+
+    try:
+        validate_df= CURRENT_PRICE_FILE_SCHEMA.validate(df,lazy=True)
+        logger.info(f"Pandera Validation successfully passed.")
+    except pa.errors.SchemarErrors as err:
+        logger.error(f" Pandera validation Failed")
+        logger.error(err.failure_cases)
+        raise
+
+    #Convert ticker to uppercase for consistency purposes
+
+    validate_df[DATA_COLS['ticker']]= validate_df[DATA_COLS['ticker']].str.upper().str.strip()
+
+
+    #Drop Nans in Market_cap
+    validate_df= validate_df.dropna(subset=['market_cap'])
+
+    logger.info(f"Final validated dataset: {len(validate_df)} rows after dropping Nans.")
+
+    return validate_df
+
+
+if __name__ == "__main__":
+    from config.logging_config import setup_logging
+    from etl_pipeline.src.extract._download_nasdaq_list import load_nasdaq_data
+    from etl_pipeline.src.extract._clean_nasdaq_list import(
+        validate_top_300,
+        extract_columns,
+        validateInData,
+        normalize_names,
+        build_master_list,
+        match_and_categorize,
+        get_top_300
+    )
+
+    setup_logging()
+    logger.info("Starting to Fecth Dividend Per Share Data=====")
+
+    #Extract + Prep
+    df = load_nasdaq_data()
+    df = validateInData(df)
+    df = extract_columns(df)
+    df = normalize_names(df)
+    master = build_master_list(df)
+    df = match_and_categorize(df, master)
+
+    top_300 = get_top_300(df)
+    top_300 = validate_top_300(top_300)
+
+    #Fetch dividend per share prices  + Process
+    tickers = validate_top_300(top_300)
+    validated_tickers = validate_dividend_tickers(tickers)
+    print(validated_tickers)
+    
+
