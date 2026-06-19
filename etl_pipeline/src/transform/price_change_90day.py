9d7619490c86505030df0ef0128facf17f4bc1f1
Arthur <arthurndubi5@gmail.com>
2026-06-19 23:40:32 +0300

    Joining backfill file and classification file to calculate price change.

--- etl_pipeline/src/transform/price_change_90day.py
+++ etl_pipeline/src/transform/price_change_90day.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from functools import reduce
+from edgar import set_identity
 
 
 from config.logging_config import(
@@ -66,6 +68,82 @@ def validating_two_dataframes(df,
     logger.info(f" PASSED: All validation for both historical data and classification data.")
     return True 
 
+
+def price_change_calculation(classified_data, historical_data):
+    #historical_data,
+    """
+    Calculate how much a stock has moved in the last 90 days.
+    """
+
+    #Take the most recent row per ticker from classified data from the data.
+    classified_data = (
+        classified_data
+        .sort_values("date")
+        .drop_duplicates(subset='ticker', keep="last")
+    )
+
+    logger.info(classified_df)
+
+    # Take the oldest row per ticker from backfill.py
+
+    historical_data = (
+        historical_data
+        .sort_values("date")
+        .drop_duplicates(subset='ticker', keep='last')
+    )
+
+    # Renaming columbs in both files
+
+    classified_data  = classified_data.rename(columns={
+        "adj_close" : "current_adjclose"
+    })
+
+    historical_data = historical_data.rename(columns={
+        "adjclose" : "historical_adjclose"
+    })
+
+    #dropping columns in the backfill.py file
+    historical_data = historical_data.drop(columns=[
+        'open','high','low','close','volume',
+        'coverage_pct','is_flagged'
+    ])
+
+    dfs=[
+        classified_data,
+        historical_data
+    ]
+
+    #Prehandle the schemas and data types before reductions begins
+    #Ensure the join key is strictly the same across all the dataset since it is a string to prevent bugs
+
+    def sanitize_dataframe(df):
+        if 'ticker' in df.columns:
+            df['ticker'] = df['ticker'].astype(str)
+
+        return df 
+    
+    #Apply sanitization to all dataframes
+    price_change_df= [sanitize_dataframe(df) for df in dfs]
+
+    try:
+        price_change_table = reduce(
+            lambda left, right: pd.merge(
+                left,
+                right, 
+                on= 'ticker',
+                how='left'
+            ),
+            price_change_df
+        )#Clean up any duplicated columns generated during the loop
+        price_change_table= price_change_table.loc[:, ~price_change_table.columns.str.endswith('_dup')]
+
+    except TypeError:
+        logger.error("Error: The dataframe list was empty")
+        price_change_table= pd.DataFrame() #Fallback
+
+    return price_change_table
+
+
 if __name__ == "__main__":
     historical_df = get_historical_data()
     classified_df = get_classified_ticker_df()
@@ -76,6 +154,9 @@ if __name__ == "__main__":
 
     for name, df in dataframes.items():
         validating_two_dataframes(df,name)
+
+    merged_table = price_change_calculation(classified_df,historical_df)
+    print(merged_table)
 
 
 
