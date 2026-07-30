578d62ac40b54d26ca2107ec422ae5067833eafb
Arthur <arthurndubi5@gmail.com>
2026-07-30 11:20:34 +0300

    Added transaction scope on the session function to handle rollbacks on errors and session cleanup.

--- database/session.py
+++ database/session.py
@@ -16,12 +16,19 @@ SessionLocal = sessionmaker(
 @contextmanager
 def get_session():
     """
+    Provide a transactional scope around a series of operations.
+    Handles commit on success, rollback on error, and sesison cleanup
     Create and return a new SQLAlchemy Session."""
 
     session=SessionLocal()
 
     try:
         yield session 
+        session.commit() #commit if no excepitions are raised
+
+    except Exception as e:
+        session.rollback() #Roll back all staged changes if an error occurs
+        raise e #Re-raise exception so caller/pipeline is aware of the failure
 
     finally:
         session.close()
