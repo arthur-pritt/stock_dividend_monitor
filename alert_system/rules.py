1673fd93687cefa8e989698aa6cf48ca4deb635f
Arthur <arthurndubi5@gmail.com>
2026-08-11 11:28:12 +0300

    SKYROCKET & DROP dataset alert created Successfully.

--- alert_system/rules.py
+++ alert_system/rules.py
@@ -14,23 +14,46 @@ logger= get_logger(__name__)
 def filtered_watchlist_df(watchlist_df:pd.DataFrame)->pd.DataFrame :
     """
     - Takes the watchlist dataframe function.
-    - Filteres the watchllst_status column to have SKYROCKET & DROP olcumn
+    - Filteres the watchllst_status column to have SKYROCKET & DROP column
     - Returns  ONLY the whole dataframe associated with SKYROCKET & DROP records."""
 
     # Confirm first if watchlist_status exist. If not raise an error/fail loudly
 
     if "watchlist_status" not in watchlist_df.columns:
-        raise ValueError(f" {watchlist_df} is missing a watchlist_status column")
+        raise ValueError(f"The dataframe is missing a watchlist_status column")
 
     logger.info(f"Watchlist_column exist in the dataframe. PROCEED")
 
-    #Filret the status column using isin() method and  flag data quality issue with logger.warning
+    #Filter unknown categorees in the dataset isin() method and  flag data quality issue with logger.warning
 
-    filtred_categories= watchlist_df[watchlist_df['watchlist_status'].isin(['SKYROCKET','DROP'])]
-    print(filtred_categories[0:50])
+    allowed_statuses=['SKYROCKET', 'NORMAL', 'DROP']
+    alert_statuses=['SKYROCKET','DROP']
 
+    unknown_categories_df= watchlist_df[~watchlist_df['watchlist_status'].isin(allowed_statuses)].copy()
+    
 
-    return watchlist_df
+    alert_df=watchlist_df[watchlist_df['watchlist_status'].isin(alert_statuses)].copy()
+    logger.info(f" Alert Status has {alert_df.shape[0]} record(s)")
+
+    if unknown_categories_df.empty:
+        logger.info(f"No unknown categorees in the dataset. PROCEED")
+        
+    else:
+        logger.warning(f"\n=== Unknown categories exist===")
+        logger.warning(f"Unknown categories has {unknown_categories_df.shape[0]} record(s)")
+        logger.warning(f"{unknown_categories_df}")
+
+    logger.info(f"SKYROCKET & DROP status filtered SUCCESSFULLY")
+
+    #Sort by pct_change in descending order.
+    alert_df=alert_df.sort_values(
+        by=['pct_change'],
+        ascending=False,
+        ignore_index=True
+    )
+
+    logger.info(f"\nSKYROCKET & DROP dataset sorted SUCCESSFULLY")
+    return alert_df
 
 if __name__ == "__main__":
     logger.info(f"Starting the filtering process...")
